926712ccd8590524ae4a1f4a84ab35f0f410bd6d
Umar Khtab <umarkhtab.te@gmail.com>
2025-01-07 17:24:13 +0500

    Add Mian Plugin Root file

---
+++ interactive-weather.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Plugin Name: Interactive Weather Dashboard
+Description: Displays a 5-day weather forecast with dynamic styling and filtering. Add this shortcode "[interactive_weather]".
+Version: 1.0
+Author: Hassan Ahmad Usmani
+*/
+
+// Define constants
+define('IW_PLUGIN_DIR', plugin_dir_path(__FILE__));
+
+// Include core files
+require_once IW_PLUGIN_DIR . 'includes/class-weather-api.php';
+require_once IW_PLUGIN_DIR . 'includes/class-weather-shortcode.php';
+
+// Initialize shortcodes
+add_action('init', ['WeatherShortcode', 'init']);
+
+// Clear all cache on deactivation
+register_deactivation_hook(__FILE__, 'IW_plugin_clear_transients');
+
+function IW_plugin_clear_transients() {
+    wp_cache_flush();
+}
+
